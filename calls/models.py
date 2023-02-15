324d6494502bd43f08fbf94e06ba1fb2bf2e6e12
Raisson Carvalho <raissoncarvalho94@gmail.com>
2023-02-15 16:39:15 -0300

    criando models para aplicacao calls

--- calls/models.py
+++ calls/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+"""  CLIENTE  """
+class Client(models.Model):
+  name = models.CharField(max_length=50, null=False, blank=False)
+  address = models.CharField(max_length=60, null=False, blank=False)
+  phone = models.CharField(max_length=9, null=False, blank=False)
+
+  def __str__(self):
+    return self.name
+
+
+
+"""  SERVIÇO  """
+class Service(models.Model):
+  value = models.FloatField(null=False)
+  description = models.CharField(max_length=30, null=False, blank=False)
+
+  def __str__(self) -> str:
+    return self.description
+
+
+""" ATENDIMENTO """
+class Order(models.Model):
+  STATUS_CHOICES = (
+    ('Pendente', 'Pendente'),
+    ('Realizado', 'Realizado'),
+    ('Cancelado', 'Cancelado'),
+  )
+  PAYMENT_CHOICES = (
+    ('Débito', 'Débito'),
+    ('Crédito', 'Crédito'),
+    ('Em espécie', 'Em espécie'),
+    ('PIX', 'PIX')
+  )
+
+  client = models.ForeignKey(Client, on_delete=models.SET_NULL, related_name='order_client', null=True)
+  services = models.ManyToManyField(Service)
+
+  status = models.CharField(max_length=10, choices=STATUS_CHOICES, null=False, default='Pendente', auto_created='Pendente')
+  payment = models.CharField(max_length=20, choices=PAYMENT_CHOICES) 
+
+  service_date = models.DateTimeField(null=False, blank=False)
+  order_registration_date = models.DateTimeField(auto_now_add=True, null=False)
