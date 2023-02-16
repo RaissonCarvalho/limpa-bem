bc60f3763a57f4964250ed65c1148e2159701a85
Raisson Carvalho <raissoncarvalho94@gmail.com>
2023-02-15 23:08:37 -0300

    implemetando listagem e cadastro de clientes

--- calls/urls.py
+++ calls/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('clients/', views.clients, name='clients'),
+    path('clients/', views.ClientsTemplateView.as_view(), name='clients-list'),
+    path('clients-add/', views.ClientsFormView.as_view(), name='clients-add'),
+
 ]
--- calls/views.py
+++ calls/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView, FormView
+from .models import Client
+from .forms import ClientForm
+from django.urls import reverse_lazy
 
-def clients(request):
-  return render(request, 'clients.html')
+class ClientsTemplateView(TemplateView):
+  template_name = 'clients/clients.html'
+  
+  def get_context_data(self, **kwargs):
+    context = super(ClientsTemplateView, self).get_context_data(**kwargs)
+    context['clients'] = Client.objects.all()
+
+    return context
+
+class ClientsFormView(FormView):
+    template_name = 'clients/clients_add.html'
+  
+    form_class = ClientForm
+
+    success_url = reverse_lazy('clients-list')
+
+    def form_valid(self, form):
+      form.save()
+      return super().form_valid(form) 
